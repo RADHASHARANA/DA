888c2d65641f4f60c5212ab369a44a9f6aea626a
RADHASHARANA <radhasharana927@gmail.com>
2023-09-14 11:25:23 +0530

    Bubble Sort

--- Programming/DSA/Sorting/Bubble_Sort/Bubble_Sort.py
+++ Programming/DSA/Sorting/Bubble_Sort/Bubble_Sort.py
@@ -1,14 +1,14 @@
 from typing import List
 
+### Ascending order
 def bubble_sort_ascending(array: List[int]):
      # Iterate from 0 to n-1
     for i in range(len(array)):
         #(Ai, Ai+1) compare Ai and Ai+1 from 0 to n-i-2 
         for j in range(0,len(array)-1-i):
             # compare Ai,Ai+1 and if not in correct position then swap
-            if array[j] > array[j+1]:
+            if array[j] > array[j+1]: # For descending change to <
                 array[j], array[j+1] = array[j+1], array[j]
-
 
 def main():
     array = list(map(int, input('Enter the unsorted array: ').split( )))
